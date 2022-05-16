73a5c405e52ddebbfcef72675aa7d83fc246a16d
Oleg Evstifeev <evstifeevoleg1989@gmail.com>
2022-05-16 09:17:16 +0300

    done

--- js/main.js
+++ js/main.js
@@ -51,25 +51,68 @@ var reviewsSwiper = new Swiper(".reviewsSwiper", {
 ** модальное окно - заявка
 ============================================== */
 jQuery(document).ready(function($){
-    $('.js-call_open').click(function(e) {
+    $('.js-popup_open').click(function (e) {
         event.preventDefault();
-        $('.popup').addClass('popup_open')
-        $('body').addClass('body_popup-open')
+        $('.popup').addClass('popup_open');
+        $('body').addClass('body_popup-open');
+
     });
+
+    $('.js-btn_call').click(function() {
+        $('.js-popup_head').html('Обратный <span>звонок</span>');
+        $('.js-text_sale').text ('Оставьте заявку и мы обязательно свяжемся с вами')
+        $('.js-form-name').value('Обратный звонок');
+    });
+
+    $('.js-btn_audit').click(function() {
+        $('.js-popup_head').html('Получить аудит <span>бесплатно</span>');
+        $('.js-text_sale').text ('Оставьте заявку и мы обязательно свяжемся с вами')
+        $('.js-form-name').value('Аудит бесплатно');
+    });
+
+    $('.js-btn_sail').click(function() {
+        $('.js-popup_head').html('Акция для новых <span>клиентов!</span>');
+        $('.js-text_sale').text ('Первый месяц обслуживания бесплатно, удаленный доступ в 1С и экспресс аудит в подарок')
+        $('.js-form-name').value('Акция');
+    });
+
+    $('.js-btn_consultation').click(function() {
+        $('.js-popup_head').html('Получить <span>консультацию!</span>');
+        $('.js-text_sale').text ('Заполните форму и получите консультацию бесплатно')
+        $('.js-form-name').value('Акция');
+    });
+
 
     $('.js-popup_close').click(function(e) {
         e.preventDefault();
         $('.popup').removeClass('popup_open');
-        $('body').removeClass('body_popup-open')
+        $('body').removeClass('body_popup-open');
     });
 
     $(document).mouseup(function(e) {
         const popup = $('.popup__callBody');
         if (e.target != popup[0] && popup.has(e.target).length === 0) {
             $('.popup').removeClass('popup_open');
-            $('body').removeClass('body_popup-open')
+            $('body').removeClass('body_popup-open');
         }
     });
+});
+
+
+$('.js-full_burger-open').click(function (e) {
+    event.preventDefault();
+    $('.popupFull').addClass('popupFull_open');
+    $('.popupFull__content_top').addClass('popupFull__content_topOpen')
+    $('.popupFull__content_bottom').addClass('popupFull__content_bottomOpen')
+    $('.popupFull__close').addClass('popupFull__close_open')
+});
+
+$('.popupFull__close').click(function(e) {
+    e.preventDefault();
+    $('.popupFull').removeClass('popupFull_open');
+    $('.popupFull__content_top').removeClass('popupFull__content_topOpen')
+    $('.popupFull__content_bottom').removeClass('popupFull__content_bottomOpen')
+    $('.popupFull__close').removeClass('popupFull__close_open')
 });
 /* ==============================================
 ** прокрутка к якорю
@@ -128,3 +171,14 @@ $('.js-fancybox-video').fancybox({
         media: {}
     }
 });
+
+
+/* ==============================================
+** Маска
+============================================== */
+$('.js-phoneMask').mask("+7 (999) 999 - 9999",{autoclear: false});
+
+
+
+
+
